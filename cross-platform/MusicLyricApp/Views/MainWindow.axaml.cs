3d21e1c0ee514e95c7d78fc369c0f347a2720c32
jitwxs <jitwxs@foxmail.com>
2025-06-02 16:13:17 +0800

    move winform version to archive dir && initial cross-platform version

---
+++ cross-platform/MusicLyricApp/Views/MainWindow.axaml.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Platform.Storage;
+using MsBox.Avalonia;
+using MsBox.Avalonia.Enums;
+using MusicLyricApp.Core.Service;
+using MusicLyricApp.ViewModels;
+
+namespace MusicLyricApp.Views;
+
+public partial class MainWindow : Window, IWindowProvider
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        DataContext = new MainWindowViewModel(this);
+    }
+    
+    protected override void OnClosing(WindowClosingEventArgs e)
+    {
+        base.OnClosing(e);
+
+        if (DataContext is not MainWindowViewModel vm) return;
+        
+        // 阻止当前关闭流程，异步处理确认
+        e.Cancel = true;
+        
+        var box = MessageBoxManager.GetMessageBoxStandard("退出提示", "你确定要退出吗？", ButtonEnum.YesNo);
+        box.ShowAsync().ContinueWith(task =>
+        {
+            if (task.Result == ButtonResult.Yes)
+            {
+                if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime lifetime)
+                {
+                    vm.SaveConfig();
+                    lifetime.Shutdown();
+                }
+            }
+        });
+    }
+
+    public async Task<IReadOnlyList<IStorageFolder>> OpenFolderPickerAsync(FolderPickerOpenOptions options)
+    {
+        return await StorageProvider.OpenFolderPickerAsync(options);
+    }
+
+    public async Task SetTextAsync(string? text)
+    {
+        await Clipboard?.SetTextAsync(text)!;
+    }
+}
